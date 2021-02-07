6898ccd346134969cc6a84019aec59736235f782
arctronic <shihabsikder98@gmail.com>
2021-02-08 02:06:04 +0600

    again initiated

---
+++ app/src/main/java/com/example/cypherchat/Chats.java
@@ -0,0 +1,22 @@
+package com.example.cypherchat;
+
+public class Chats {
+
+    public String date;
+
+    public Chats(){
+
+    }
+
+    public Chats(String date){
+        this.date = date;
+    }
+
+    public String getDate(){
+        return date;
+    }
+
+    public void setDate(String date){
+        this.date = date;
+    }
+}
